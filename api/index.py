47ce681b28517a76b5471db2bb71dc4ecbf7f699
Erwin Lejeune <erwin.lejeune@tii.ae>
2025-01-05 16:49:16 +0400

    respect coingecko api

--- api/index.py
+++ api/index.py
@@ -22,8 +22,8 @@ TOKEN_DECIMALS = int(os.getenv("TOKEN_DECIMALS", 18))
 API_VERSION = os.getenv("API_VERSION", "v1")
 ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
 CHAIN_IDS = [int(chain) for chain in os.getenv("CHAIN_IDS", "").split(",")]
-CACHE_DURATION = int(os.getenv("CACHE_DURATION", 900))  # Default to 15 minutes
-RATE_LIMIT = os.getenv("RATE_LIMIT", "5 per minute")  # Default to 5 requests per minute
+CACHE_DURATION = int(os.getenv("CACHE_DURATION", 1790))  # Default to 15 minutes
+RATE_LIMIT = os.getenv("RATE_LIMIT", "1 per minute")  # Default to 5 requests per minute
 
 # Configure caching
 cache = Cache(app, config={"CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": CACHE_DURATION})
@@ -36,6 +36,7 @@ ETHERSCAN_BASE_URL = "https://api.etherscan.io/v2/api"
 
 # Semaphore for rate limiting (5 calls per minute)
 semaphore = asyncio.Semaphore(5)
+
 async def fetch_balance(session, address, chain_id):
     """
     Fetch token balance for a given address and chain ID from Etherscan API.
@@ -73,28 +74,26 @@ async def fetch_all_balances(addresses, chains):
                 tasks.append(fetch_balance(session, address, chain_id))
         return await asyncio.gather(*tasks)
 
+def calculate_circulating_supply():
+    """
+    Calculate the circulating supply by aggregating locked supply.
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    balances = loop.run_until_complete(fetch_all_balances(LOCKED_ADDRESSES, CHAIN_IDS))
+
+    # Aggregate locked supply
+    locked_supply = sum(balances)
+    locked_supply /= 10 ** TOKEN_DECIMALS
+    return TOTAL_SUPPLY - locked_supply
+
 @cache.cached(timeout=CACHE_DURATION, key_prefix="circulating_supply")
 @limiter.limit(RATE_LIMIT)  # Apply rate limit
 @app.route(f'/', methods=['GET'])
-def get_circulating_supply():
+def get_simple_circulating_supply():
     try:
-        # Run asyncio event loop
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        balances = loop.run_until_complete(fetch_all_balances(LOCKED_ADDRESSES, CHAIN_IDS))
-
-        # Aggregate locked supply
-        locked_supply = sum(balances)
-        locked_supply /= 10 ** TOKEN_DECIMALS
-        circulating_supply = TOTAL_SUPPLY - locked_supply
-
-        return jsonify({
-            "api_version": API_VERSION,
-            "total_supply": TOTAL_SUPPLY,
-            "locked_supply": locked_supply,
-            "circulating_supply": circulating_supply,
-            "chains": CHAIN_IDS
-        })
+        circulating_supply = calculate_circulating_supply()
+        return jsonify({"result": circulating_supply, "lock_contracts": LOCKED_ADDRESSES})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
